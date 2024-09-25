06d58be7867aae2f7ec35f17df3b5a3727e801cc
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-09-25 14:55:39 +0200

    Create post component

---
+++ src/pages/Post.tsx
@@ -0,0 +1,54 @@
+// import { useNavigate, useParams } from "react-router-dom";
+// import PostItem from "../components/PostItem";
+// import { useEffect, useState } from "react";
+// import { toLink } from "../routes";
+
+const Post = () => {
+    // const navigate = useNavigate();
+    // const { page } = useParams();
+
+    // useEffect(() => {
+    //     let pageNumber: number = 0;
+
+    //     if (typeof page !== "undefined") {
+    //         pageNumber = parseInt(page);
+    //     }
+
+    //     if (
+    //         (pageNumber === 0 || pageNumber.toString() !== page) &&
+    //         location.pathname !== toLink("home")
+    //     ) {
+    //         navigate(toLink("home"));
+    //     }
+    // }, [page, navigate]);
+
+    // const [posts, setPosts] = useState([]);
+
+    // useEffect(() => {
+    //     fetch("http://localhost:3000/posts")
+    //         .then((res) => res.json())
+    //         .then((data) => setPosts(data));
+    // }, []);
+
+    return (
+        <>
+            {/* <h1 className="text-4xl text-center mb-10 font-bold">Blog Posts</h1>
+
+            <div
+                className={`grid ${
+                    Object.keys(posts).length !== 0 ? "md:grid-cols-2" : ""
+                } gap-x-8 gap-y-10`}
+            >
+                {Object.keys(posts).length !== 0 ? (
+                    posts.map((post, id) => (
+                        <PostItem post={post} key={id} showSummary={true} />
+                    ))
+                ) : (
+                    <PostItem />
+                )}
+            </div> */}
+        </>
+    );
+};
+
+export default Post;
